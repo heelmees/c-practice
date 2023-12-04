8dbf7c9466a317acafd0ac4cc7a43f961d9be078
Richard Tretyakov <richard7kl@gmail.com>
2023-12-04 16:33:01 +0200

    4.3 операторы вместо if

--- labs/task4_3.cpp
+++ labs/task4_3.cpp
@@ -13,7 +13,26 @@ int main() {
         std::cout << "Negative number" << std::endl;
     } else if (x == 1) {
         std::cout << "Positive number" << std::endl;
+    } else {
+        std::cout << "Число должно быть 1 или -1";
     }
+
+    //Также можно использовать оператор switch/case
+    switch (x)
+    {
+    case -1:
+        std::cout << "Negative number" << std::endl;
+        break;
+    case 1:
+        std::cout << "Positive number" << std::endl;
+        break;
+    default:
+        std::cout << "Число должно быть 1 или -1";
+        break;
+    }
+
+    //Также можно использовать тренарный оператор
+    std::cout << (x == -1 ? "Negative number" : (x == 1 ? "Positive number" : "Число должно быть 1 или -1"));
 
     return 0;
 }
