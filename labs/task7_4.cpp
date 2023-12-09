e26b1586ab2a8ec4e68f22efb0710a016ba9c3f9
Richard Tretyakov <richard7kl@gmail.com>
2023-12-09 13:12:47 +0200

    7.4 fix

--- labs/task7_4.cpp
+++ labs/task7_4.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <algorithm>
 
+void printIntSequence(int sequence[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << sequence[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     const int maxN = 1000;
-    int sequence[maxN];
-    int n;
+    int sequence[maxN * 2];
+    int n;  
     std::cout << "Введите количество элементов до (" << maxN << "): ";
     std::cin >> n;
 
@@ -16,20 +23,38 @@ int main() {
 
     std::sort(sequence, sequence + n);
 
-    // Удаление продублированных элементов (в задании звучало как: "Затем удалите и продублируйте элементы".)
-    int uniqueCount = 0;
-    for (int i = 0; i < n; i++) {
-        if (i == 0 || sequence[i] != sequence[i - 1]) {
-            sequence[uniqueCount++] = sequence[i];
+    // Удаление продублированных элементов
+    for (int i = 1; i < n; ++i) {
+        if (sequence[i] == sequence[i - 1]) {
+            for (int j = i; j < n - 1 ; ++j) {
+                sequence[j] = sequence[j + 1];
+            }
+            --n;
+            --i;
         }
     }
 
-    // Вывод уникальных элементов
-    std::cout << "Полученная последовательность:" << std::endl;
-    for (int i = 0; i < uniqueCount; i++) {
-        std::cout << sequence[i] << " ";
+    std::cout << "Последовательность после удаления дубликатов:" << std::endl;
+    printIntSequence(sequence, n);
+ 
+    for (int i = 0; i < n; ++i) {
+        for (int j = n - 1; j >= i; --j) {
+            sequence[j + 1] = sequence[j];
+        }
+        ++n;
+        ++i;
+    }
+    
+    std::cout << "Последовательность после дублирования:" << std::endl;
+    printIntSequence(sequence, n);
+
+    std::cout << "Итоговый результат задачи (каждый элемент по одному разу)" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        if (i == 0 || sequence[i] != sequence[i - 1]) {
+            std::cout << sequence[i] << " ";
+        }
     }
     std::cout << std::endl;
-
+    
     return 0;
 }
