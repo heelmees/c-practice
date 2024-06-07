119752fd58428039b36b2d7eb3f77b625151a29b
heelmees <148966417+heelmees@users.noreply.github.com>
2024-06-07 15:53:28 +0200

    лаба 7

---
+++ labs_2/task_7/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <SFML/Graphics.hpp>
+#include <Game.hpp>
+#include <circle.hpp>
+
+
+int main()
+{
+    mt::Game game(1000, 600, "Game");
+
+    if (!game.Setup(10))
+        return -1;
+
+    game.LifeCycle();
+
+    return 0;
+}
